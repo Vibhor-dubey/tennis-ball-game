77973d3d4dde49b7b39e2df6963e66a793287d31
Vibhor-dubey <72804572+Vibhor-dubey@users.noreply.github.com>
2020-11-03 19:57:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine=Matter.Engine
+const Bodies=Matter.Bodies
+const World= Matter.World
+
+var engine,world;
+var ground,ball
+
+function setup() {
+  createCanvas(400,400);
+engine=Engine.create();
+world=engine.world;
+var ground_options={
+  isStatic:true
+}
+ground=Bodies.rectangle(200,390,400,20,ground_options);
+World.add(world,ground);
+var ball_options={
+restitution:1
+}
+ball=Bodies.circle(250,200,10,ball_options);
+World.add(world,ball);
+}
+function draw() {
+  background(0,0,0); 
+Engine.update(engine);
+   rectMode(CENTER);
+   rect(ground.position.x,ground.position.y,400,20);
+ellipseMode(RADIUS);
+ellipse(ball.position.x,ball.position.y,10);
+ 
+}
+
+
